56b392e35c81dba11370cc3de15c382fc26c92be
kory <62596617+korylee@users.noreply.github.com>
2026-05-09 13:41:42 +0800

    chore: 添加格式化与测试配置

---
+++ vitest.config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from "vitest/config";
+import path, { resolve } from "node:path";
+import solid from "vite-plugin-solid";
+
+export default defineConfig({
+  plugins: [solid({ hot: !process.env.VITEST })],
+  resolve: {
+    alias: {
+      "@s-primitives/core": path.resolve(import.meta.dirname, "packages/core"),
+      "@s-primitives/shared": path.resolve(
+        import.meta.dirname,
+        "packages/shared",
+      ),
+    },
+    dedupe: ["solid-js"],
+  },
+  cacheDir: resolve(import.meta.dirname, "node_modules/.vite"),
+  test: {
+    reporters: "dot",
+    exclude: ["**/node_modules/**", "**/dist/**"],
+    coverage: {
+      include: ["packages/**/*.{ts,tsx}"],
+      exclude: [
+        "**/.test/**",
+        "**/dist/**",
+        "**/types.ts",
+        "**/*.config.{ts,tsx}",
+      ],
+    },
+    projects: [
+      "packages/*/vitest.config.ts",
+      {
+        extends: "./vitest.config.ts",
+        test: {
+          name: "unit",
+          environment: "jsdom",
+          setupFiles: [resolve(import.meta.dirname, "packages/.test/setup.ts")],
+          include: [
+            "packages/**/*.{test,spec}.{ts,tsx}",
+            "test/*.{test,spec}.{ts,tsx}",
+          ],
+          exclude: ["packages/**/*.{browser,server}.{test,spec}.{ts,tsx}"],
+          server: {
+            deps: {
+              inline: ["solid-js"],
+            },
+          },
+        },
+      },
+    ],
+  },
+});
